9e51dfbdf160e2d0dd524ae1dc7a8ee9af2949b5
jyhwang <stratojy@gmail.com>
2025-05-14 20:18:40 +0900

    feat. click  이벤트로 좋아요 버튼 만들기

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
     <p>개봉: {{ movie.year }}</p>
     <p>장르: {{ movie.category }}</p>
-    <button v-on:click="increaseLike">좋아요</button><span>{{ like }}</span>
+    <button v-on:click="increaseLike(i)">좋아요</button><span>{{ movie.like }}</span>
   </div>
   <!-- <p v-for="(item, i) in foods" :key="i">{{ item }}</p> -->
 </template>
@@ -17,15 +17,15 @@ export default {
       like: 0,
       // foods: ["김밥", "순대", "만두", "떡볶이"],
       data: [
-        { title: "노량", year: 2023, category: "액션, 드라마", textRed: "color: red" },
-        { title: "아쿠아맨과 로스트 킹덤", year: 2023, category: "액션, 판타지, 어드밴처" },
-        { title: "3일의 휴가", year: 2023, category: "판타지, 드라마" },
+        { title: "노량", year: 2023, category: "액션, 드라마", textRed: "color: red", like:0},
+        { title: "아쿠아맨과 로스트 킹덤", year: 2023, category: "액션, 판타지, 어드밴처", like:0 },
+        { title: "3일의 휴가", year: 2023, category: "판타지, 드라마", like:0 },
       ],
     };
   },
   methods: {
-    increaseLike() {
-      this.like += 1;
+    increaseLike(i) {
+      this.data[i].like += 1;
     },
   },
 };
