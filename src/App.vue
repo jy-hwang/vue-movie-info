e0a4fddb7acdd9de7165b23c9f7b40275e4dea39
jyhwang <stratojy@gmail.com>
2025-05-15 01:16:27 +0900

    feat. 영화 목록을 component 로 만들기
    1. html 태그와 css 소스를 component 로 분리
    2. parent -> child : props
    3. child -> parent : emit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
 <template>
   <Navbar />
   <Event :text="text" />
-  <h1>영화정보</h1>
-  <div v-for="(movie, i) in data" :key="i" class="item">
-    <figure>
-      <img :src="`${movie.imgUrl}`" :alt="movie.title" />
-    </figure>
-    <div class="info">
-      <h3>{{ movie.title }}</h3>
-      <p>개봉: {{ movie.year }}</p>
-      <p>장르: {{ movie.category }}</p>
-      <button v-on:click="increaseLike(i)">좋아요</button
-      ><span>{{ movie.like }}</span>
-      <p>
-        <button
-          @click="
-            isModal = true;
-            selectedMovie = i;
-          "
-        >
-          상세보기
-        </button>
-      </p>
-    </div>
-  </div>
+  <Movies
+    :data="data"
+    @openModal="
+      isModal = true;
+      selectedMovie = $event;
+    "
+    @increaseLike="increaseLike($event)"
+  />
+
   <Modal
     :data="data"
     :isModal="isModal"
@@ -37,6 +23,7 @@ import movieData from "./assets/movies";
 import Navbar from "./components/Navbar.vue";
 import Modal from "./components/Modal.vue";
 import Event from "./components/Event.vue";
+import Movies from "./components/Movies.vue";
 
 export default {
   name: "App",
@@ -57,6 +44,7 @@ export default {
     Navbar: Navbar,
     Modal: Modal,
     Event: Event,
+    Movies: Movies,
   },
 };
 </script>
@@ -86,26 +74,5 @@ p {
 button {
   margin-right: 10px;
   margin-top: 1rem;
-}
-
-.item {
-  width: 100%;
-  border: 1px solid #ccc;
-  display: flex;
-  margin-bottom: 20px;
-  padding: 1rem;
-}
-
-.item figure {
-  width: 30%;
-  margin-right: 1rem;
-}
-
-.item img {
-  width: 100%;
-}
-
-.item .info {
-  width: 100%;
 }
 </style>
