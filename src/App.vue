232e3dfd4c8b482713845d8cb2795c2cb925314f
jy-hwang <stratojy@gmail.com>
2025-05-14 18:16:54 +0900

    feat. vue 맛보기 시작
    1. App.vue 파일 수정해서 맛보기
    2. 프로젝트 및 파일 구조 확인하기
    3. template 에 html 소스를 작성하고, script 쪽에 data를 넣고 binding or mapping

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>영화정보</h1>
+  <div v-for="(movie, i) in data" :key="i">
+    <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
+    <p>개봉: {{ movie.year }}</p>
+    <p>장르: {{ movie.category }}</p>
+    <button v-on:click="increaseLike">좋아요</button><span>{{ like }}</span>
+  </div>
+  <!-- <p v-for="(item, i) in foods" :key="i">{{ item }}</p> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      like: 0,
+      // foods: ["김밥", "순대", "만두", "떡볶이"],
+      data: [
+        { title: "노량", year: 2023, category: "액션, 드라마", textRed: "color: red" },
+        { title: "아쿠아맨과 로스트 킹덤", year: 2023, category: "액션, 판타지, 어드밴처" },
+        { title: "3일의 휴가", year: 2023, category: "판타지, 드라마" },
+      ],
+    };
+  },
+  methods: {
+    increaseLike() {
+      this.like += 1;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.bg-yellow {
+  background-color: yellow;
 }
 </style>
