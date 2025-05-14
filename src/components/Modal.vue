941d7975895012034942bfbee651bb0c4a80d63b
jyhwang <stratojy@gmail.com>
2025-05-14 23:49:16 +0900

    feat. component로 기능별 UI 분리하기 2
    1. Modal 도 따로 분리하기

---
+++ src/components/Modal.vue
@@ -0,0 +1,34 @@
+<template lang="">
+  <div class="modal" v-if="isModal">
+    <div class="inner">
+      <h3>{{ data[selectedMovie].title }}</h3>
+      <p>영화 상세정보</p>
+      <button @click="isModal = false">닫기</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ModalComponent",
+};
+</script>
+<style>
+.modal {
+  background: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal .inner {
+  background: #fff;
+  width: 80%;
+  padding: 20px;
+  border-radius: 10px;
+}
+</style>
